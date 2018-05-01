7e4bf91f08267f09a4c6a98f40a82255c901c60b
ophid1an <ophid1an@users.noreply.github.com>
2018-05-01 10:54:50 +0300

    Use fixed random_state, use dataframes

--- Erwtima1.py
+++ Erwtima1.py
@@ -1,6 +1,6 @@
 import time
 from sklearn import datasets
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import BaggingClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -16,71 +16,52 @@ import numpy as nu
 import pandas as pa
 import scipy
 
-data1="./Datasets/iris.csv"
-data2="./Datasets/wine.csv"
-data3="./Datasets/wdbc.csv"
-data4="./Datasets/balance-scale.csv"
-data5="./Datasets/hayesroth.csv"
-data6="./Datasets/haberman.csv"
-data7="./Datasets/liverdisorder.csv"
-data8="./Datasets/data_banknote_authentication.csv"
-data9="./Datasets/ionosphere.csv"
-data10="./Datasets/cmc.csv"
+RANDOM_STATE = 0
 
-iris_data= pa.read_csv(data1) #load iris dataset
-wine_data= pa.read_csv(data2) #load wine dataset
-bcancer_data=pa.read_csv(data3)#load breast cancer dataset
-balance_data=pa.read_csv(data4)#load balance-scale dataset
-hayesroth_data=pa.read_csv(data5)#load hayes-roth dataset
-haberman_data=pa.read_csv(data6)#load haberman survival dataset
-liver_data=pa.read_csv(data7)#load Liver disorders  dataset
-bank_data=pa.read_csv(data8)#load banknote authentication dataset
-ionosphere_data=pa.read_csv(data9)#load ionosphere dataset
-cmc_data=pa.read_csv(data10)#load Contraceptive Method Choice dataset
+iris_df = pa.read_csv("./Datasets/iris.csv")  # load Iris Dataset
+wine_df = pa.read_csv("./Datasets/wine.csv")  # load Wine Dataset
+bcancer_df = pa.read_csv("./Datasets/wdbc.csv")  # load Breast Cancer Dataset
+balance_df = pa.read_csv("./Datasets/balance-scale.csv")  # load Balance Scale Dataset
+hayesroth_df = pa.read_csv("./Datasets/hayesroth.csv")  # load Hayes-Roth Dataset
+haberman_df = pa.read_csv("./Datasets/haberman.csv")  # load Haberman's Survival Dataset
+liver_df = pa.read_csv("./Datasets/liverdisorder.csv")  # load Liver Disorders Dataset
+bank_df = pa.read_csv("./Datasets/data_banknote_authentication.csv")  # load Banknote Authentication Dataset
+ionosphere_df = pa.read_csv("./Datasets/ionosphere.csv")  # load Ionosphere Dataset
+cmc_df = pa.read_csv("./Datasets/cmc.csv")  # load Contraceptive Method Choice Dataset"
 
-Data=[]
-target=[]
-#Iris data and target
-array_iris = iris_data.values
-iris_data = array_iris [:,0:3]
-iris_target = array_iris [:,4]
-#Wine data and target
-array_wine = wine_data.values
-wine_data = array_wine [:,1:12]
-wine_target = array_wine [:,0]
-#Breast cancer data and target
-array_bcancer=bcancer_data.values
-bcancer_data = array_bcancer [:,2:31]
-bcancer_target = array_bcancer [:,1]
-#Balance-scale data and target
-array_balance=balance_data.values
-balance_data= array_balance [:,1:4]
-balance_target= array_balance [:,0]
-#hayes-roth data and target
-array_hayesroth=hayesroth_data.values
-hayesroth_data= array_hayesroth [:,0:4]
-hayesroth_target= array_hayesroth [:,5]
-#Haberman survival data and target
-array_haberman=haberman_data.values
-haberman_data= array_haberman [:,0:2]
-haberman_target= array_haberman [:,3]
-#Liver Disorder  data and target
-array_liver=liver_data.values
-liver_data= array_liver [:,0:5]
-liver_target= array_liver [:,6]
-#Chess(Rook vs King) data and target
-array_bank=bank_data.values
-bank_data= array_bank[:,0:3]
-bank_target= array_bank [:,4]
-#Ionosphere data and target
-array_ionosphere=ionosphere_data.values
-ionosphere_data= array_ionosphere[:,0:33]
-ionosphere_target= array_ionosphere[:,34]
-#cmc data and target
-array_cmc=cmc_data.values
-cmc_data= array_cmc[:,0:8]
-cmc_target= array_cmc[:,9]
-#Data is the table with all dataset data
+Data = []
+target = []
+# Iris data and target
+iris_data = iris_df.iloc[:, 0:3]
+iris_target = iris_df.iloc[:, 4]
+# Wine data and target
+wine_data = wine_df.iloc[:, 1:12]
+wine_target = wine_df.iloc[:, 0]
+# Breast cancer data and target
+bcancer_data = bcancer_df.iloc[:, 2:31]
+bcancer_target = bcancer_df.iloc[:, 1]
+# Balance-scale data and target
+balance_data = balance_df.iloc[:, 1:4]
+balance_target = balance_df.iloc[:, 0]
+# hayes-roth data and target
+hayesroth_data = hayesroth_df.iloc[:, 0:4]
+hayesroth_target = hayesroth_df.iloc[:, 5]
+# Haberman survival data and target
+haberman_data = haberman_df.iloc[:, 0:2]
+haberman_target = haberman_df.iloc[:, 3]
+# Liver Disorder  data and target
+liver_data = liver_df.iloc[:, 0:5]
+liver_target = liver_df.iloc[:, 6]
+# Chess(Rook vs King) data and target
+bank_data = bank_df.iloc[:, 0:3]
+bank_target = bank_df.iloc[:, 4]
+# Ionosphere data and target
+ionosphere_data = ionosphere_df.iloc[:, 0:33]
+ionosphere_target = ionosphere_df.iloc[:, 34]
+# cmc data and target
+cmc_data = cmc_df.iloc[:, 0:8]
+cmc_target = cmc_df.iloc[:, 9]
+# Data is the table with all dataset data
 Data.append(iris_data)
 Data.append(wine_data)
 Data.append(bcancer_data)
@@ -91,7 +72,7 @@ Data.append(liver_data)
 Data.append(bank_data)
 Data.append(ionosphere_data)
 Data.append(cmc_data)
-#Target is the table with all dataset targets
+# Target is the table with all dataset targets
 target.append(iris_target)
 target.append(wine_target)
 target.append(bcancer_target)
@@ -102,41 +83,42 @@ target.append(liver_target)
 target.append(bank_target)
 target.append(ionosphere_target)
 target.append(cmc_target)
-#Table with the labels for each dataset
-Data_label=["Iris dataset","Wine Dataset","Breast Cancer Dataset","Balance-Scale Dataset","Hayes-Roth Dataset",
-            "Haberman Survival Dataset","Liver Disorder Dataset","Bank-Note Dataset","Ionosphere Dataset","Contraceptive Method Choice Dataset"]
+# Table with the labels for each dataset
+Data_label = ["Iris Dataset", "Wine Dataset", "Breast Cancer Dataset", "Balance Scale Dataset", "Hayes-Roth Dataset",
+              "Haberman's Survival Dataset", "Liver Disorders Dataset", "Banknote Authentication Dataset",
+              "Ionosphere Dataset",
+              "Contraceptive Method Choice Dataset"]
 
 classifiers = []
-#Tree Classifier
-dt = tree.DecisionTreeClassifier()
+# Tree Classifier
+dt = DecisionTreeClassifier(random_state=RANDOM_STATE)
 classifiers.append([dt, "tree"])
 
-#-----Manipulating the training examples-------
-#Bagging
-bagged_dt = BaggingClassifier(tree.DecisionTreeClassifier(),n_estimators=100)
+# -----Manipulating the training examples-------
+# Bagging
+bagged_dt = BaggingClassifier(dt, n_estimators=100, random_state=RANDOM_STATE)
 classifiers.append([bagged_dt, "bagged tree"])
-#Boosting
-ada = AdaBoostClassifier(base_estimator=tree.DecisionTreeClassifier(),n_estimators=100)
+# Boosting
+ada = AdaBoostClassifier(base_estimator=dt, n_estimators=100, random_state=RANDOM_STATE)
 classifiers.append([ada, "AdaBoost-ed tree"])
 
-gbm = GradientBoostingClassifier(n_estimators=100)
+gbm = GradientBoostingClassifier(n_estimators=100, random_state=RANDOM_STATE)
 classifiers.append([gbm, "gradient boosting tree"])
-#-----Manipulating the target variable------
-#One vs One
-ovo = OneVsOneClassifier(tree.DecisionTreeClassifier())
+# -----Manipulating the target variable------
+# One vs One
+ovo = OneVsOneClassifier(dt)
 classifiers.append([ovo, "one-vs-one tre"])
-#One vs Rest
-ovr = OneVsRestClassifier(tree.DecisionTreeClassifier())
+# One vs Rest
+ovr = OneVsRestClassifier(dt)
 classifiers.append([ovr, "one-vs-rest tree"])
-#-----Injecting randomness------
-#Random forest
-rf = RandomForestClassifier()
-classifiers.append([rf,"Random forest"])
-#-----Manipulating Features------
-#RandomPatches
-bagged_dt_mf = BaggingClassifier(tree.DecisionTreeClassifier(),n_estimators=100,max_samples=0.7, max_features=0.7)
+# -----Injecting randomness------
+# Random forest
+rf = RandomForestClassifier(random_state=RANDOM_STATE)
+classifiers.append([rf, "Random forest"])
+# -----Manipulating Features------
+# RandomPatches
+bagged_dt_mf = BaggingClassifier(dt, n_estimators=100, max_samples=0.7, max_features=0.7, random_state=RANDOM_STATE)
 classifiers.append([bagged_dt_mf, "bagged tree random patches"])
-
 
 for i in range(len(Data)):
     print("------ %20s ------ " % (Data_label[i]))
@@ -144,4 +126,4 @@ for i in range(len(Data)):
         start = time.time()
         scores = cross_val_score(classifier, Data[i], target[i], cv=10)
         stop = time.time()
-        print("%20s accuracy: %0.2f (+/- %0.2f), time:%.4f" % (label, scores.mean(), scores.std() * 2, stop - start))
+        print("%20s accuracy: %0.3f (+/- %0.3f), time:%.3f" % (label, scores.mean(), scores.std() * 2, stop - start))
