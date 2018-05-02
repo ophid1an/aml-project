af64094004903b6148ef06dafac11d7649704a83
ophid1an <ophid1an@users.noreply.github.com>
2018-05-02 20:33:29 +0300

    Use array of dicts for classifiers

--- Erwtima2.py
+++ Erwtima2.py
@@ -43,104 +43,112 @@ X_cps_r, y_cps_r, cost_mat_cps_r = cost_sampling(X_train, y_train, cost_mat_trai
 X_u, y_u, cost_mat_u = undersampling(X_train, y_train, cost_mat_train)
 
 # Calibrated models
-classifiers = {
-    "RF": {"f": CalibratedClassifierCV(RandomForestClassifier(random_state=RANDOM_STATE), cv=10, method='sigmoid')},
-    "LSVM": {"f": CalibratedClassifierCV(LinearSVC(random_state=RANDOM_STATE), cv=10, method='sigmoid')},
-    "GNB": {"f": CalibratedClassifierCV(GaussianNB(), cv=10, method='sigmoid')},
-}
+classifiers = [
+    {"name": "RF", "f": CalibratedClassifierCV(RandomForestClassifier(random_state=RANDOM_STATE), cv=10,
+                                               method='sigmoid')},
+    {"name": "LSVM", "f": CalibratedClassifierCV(LinearSVC(random_state=RANDOM_STATE), cv=10, method='sigmoid')},
+    {"name": "GNB", "f": CalibratedClassifierCV(GaussianNB(), cv=10, method='sigmoid')},
+]
 
-for model in classifiers.keys():
+for clf in classifiers:
     # Fit
-    classifiers[model]["f"].fit(X_train, y_train)
+    clf["f"].fit(X_train, y_train)
     # Predict
-    classifiers[model]["c"] = classifiers[model]["f"].predict(X_test)
-    classifiers[model]["p"] = classifiers[model]["f"].predict_proba(X_test)
-    classifiers[model]["ptrain"] = classifiers[model]["f"].predict_proba(X_train)
+    clf["c"] = clf["f"].predict(X_test)
+    clf["p"] = clf["f"].predict_proba(X_test)
+    clf["ptrain"] = clf["f"].predict_proba(X_train)
     # Sampling
-    classifiers[model]["c_o"] = classifiers[model]["f"].fit(X_cps_o, y_cps_o).predict(
+    clf["c_o"] = clf["f"].fit(X_cps_o, y_cps_o).predict(
         X_test)  # Probabilities cost Oversampling
-    classifiers[model]["c_o_p"] = classifiers[model]["f"].fit(X_cps_o, y_cps_o).predict_proba(
+    clf["c_o_p"] = clf["f"].fit(X_cps_o, y_cps_o).predict_proba(
         X_test)  # Probabilities cost Oversampling
-    classifiers[model]["c_r"] = classifiers[model]["f"].fit(X_cps_r, y_cps_r).predict(
+    clf["c_r"] = clf["f"].fit(X_cps_r, y_cps_r).predict(
         X_test)  # Probabilities cost RejectionSampling
-    classifiers[model]["c_u"] = classifiers[model]["f"].fit(X_u, y_u).predict(X_test)  # Probabilities undersampling
+    clf["c_u"] = clf["f"].fit(X_u, y_u).predict(X_test)  # Probabilities undersampling
 
 measures = {"f1": f1_score, "pre": precision_score,
             "rec": recall_score, "acc": accuracy_score}
+
+my_measures = [
+    {"name": "acc", "f": accuracy_score},{"name": "pre", "f": precision_score},
+    {"name": "rec", "f": recall_score},{"name": "f1", "f": f1_score},
+]
+
 results = pa.DataFrame(columns=measures.keys())
 
 # Evaluate each model in classifiers
-for model in classifiers.keys():
-    results.loc[model] = [measures[measure](y_test, classifiers[model]["c"]) for measure in measures.keys()]
-    results.loc[model + "-O"] = [measures[measure](y_test, classifiers[model]["c_o"]) for measure in measures.keys()]
-    results.loc[model + "-R"] = [measures[measure](y_test, classifiers[model]["c_r"]) for measure in measures.keys()]
-    results.loc[model + "-U"] = [measures[measure](y_test, classifiers[model]["c_u"]) for measure in measures.keys()]
+for clf in classifiers:
+    results.loc[clf['name']] = [measures[measure](y_test, clf["c"]) for measure in measures.keys()]
+    results.loc[clf['name'] + "-O"] = [measures[measure](y_test, clf["c_o"]) for measure in measures.keys()]
+    results.loc[clf['name'] + "-R"] = [measures[measure](y_test, clf["c_r"]) for measure in measures.keys()]
+    results.loc[clf['name'] + "-U"] = [measures[measure](y_test, clf["c_u"]) for measure in measures.keys()]
 
 results["sav_score"] = np.zeros(results.shape[0])
 results["cost_loss"] = np.zeros(results.shape[0])
-for model in classifiers.keys():
-    results["sav_score"].loc[model] = savings_score(y_test, classifiers[model]["c"], cost_mat_test)
-    results["cost_loss"].loc[model] = cost_loss(y_test, classifiers[model]["c"], cost_mat_test)
+
+for clf in classifiers:
+    results["sav_score"].loc[clf['name']] = savings_score(y_test, clf["c"], cost_mat_test)
+    results["cost_loss"].loc[clf['name']] = cost_loss(y_test, clf["c"], cost_mat_test)
     # Evaluate cost Oversampling
-    results["sav_score"].loc[model + "-O"] = savings_score(y_test, classifiers[model]["c_o"], cost_mat_test)
-    results["cost_loss"].loc[model + "-O"] = cost_loss(y_test, classifiers[model]["c_o"], cost_mat_test)
+    results["sav_score"].loc[clf['name'] + "-O"] = savings_score(y_test, clf["c_o"], cost_mat_test)
+    results["cost_loss"].loc[clf['name'] + "-O"] = cost_loss(y_test, clf["c_o"], cost_mat_test)
     # Evaluate cost RejectionSampling
-    results["sav_score"].loc[model + "-R"] = savings_score(y_test, classifiers[model]["c_r"], cost_mat_test)
-    results["cost_loss"].loc[model + "-R"] = cost_loss(y_test, classifiers[model]["c_r"], cost_mat_test)
+    results["sav_score"].loc[clf['name'] + "-R"] = savings_score(y_test, clf["c_r"], cost_mat_test)
+    results["cost_loss"].loc[clf['name'] + "-R"] = cost_loss(y_test, clf["c_r"], cost_mat_test)
     # Evaluate undersampling
-    results["sav_score"].loc[model + "-U"] = savings_score(y_test, classifiers[model]["c_u"], cost_mat_test)
-    results["cost_loss"].loc[model + "-U"] = cost_loss(y_test, classifiers[model]["c_u"], cost_mat_test)
+    results["sav_score"].loc[clf['name'] + "-U"] = savings_score(y_test, clf["c_u"], cost_mat_test)
+    results["cost_loss"].loc[clf['name'] + "-U"] = cost_loss(y_test, clf["c_u"], cost_mat_test)
 
 print(results)
 
-ci_models = classifiers.keys()
-
-for model in list(ci_models):
-    # ---------------Cost Minimazation-----------------------
-    # Model BMR
-    classifiers[model + "-BMR"] = {"f": BayesMinimumRiskClassifier()}  # BMR
-    # Fit
-    classifiers[model + "-BMR"]["f"].fit(y_test, classifiers[model]["p"])
-    # Predict
-    classifiers[model + "-BMR"]["c"] = classifiers[model + "-BMR"]["f"].predict(classifiers[model]["p"], cost_mat_test)
-    # Evaluate
-    results.loc[model + "-BMR"] = 0
-    results.loc[model + "-BMR", measures.keys()] = \
-        [measures[measure](y_test, classifiers[model + "-BMR"]["c"]) for measure in measures.keys()]
-    results["sav_score"].loc[model + "-BMR"] = savings_score(y_test, classifiers[model + "-BMR"]["c"], cost_mat_test)
-    results["cost_loss"].loc[model + "-BMR"] = cost_loss(y_test, classifiers[model + "-BMR"]["c"], cost_mat_test)
-    # Model TO
-    classifiers[model + "-TO"] = {"f": ThresholdingOptimization()}  #
-    # Fit
-    classifiers[model + "-TO"]["f"].fit(classifiers[model]["ptrain"], cost_mat_train, y_train)  # Classifier TO
-    # Predict
-    classifiers[model + "-TO"]["c"] = classifiers[model + "-TO"]["f"].predict(classifiers[model]["p"])  # Classifier TO
-    # Evaluate
-    results.loc[model + "-TO"] = 0
-    results.loc[model + "-TO", measures.keys()] = \
-        [measures[measure](y_test, classifiers[model + "-TO"]["c"]) for measure in measures.keys()]
-    results["sav_score"].loc[model + "-TO"] = savings_score(y_test, classifiers[model + "-TO"]["c"], cost_mat_test)
-    results["cost_loss"].loc[model + "-TO"] = cost_loss(y_test, classifiers[model + "-TO"]["c"], cost_mat_test)
-
-classifiers["CSRP"] = {"f": CostSensitiveRandomPatchesClassifier(combination='weighted_voting')}
-# Fit
-classifiers["CSRP"]["f"].fit(X_train, y_train, cost_mat_train)
-# Predict
-classifiers["CSRP"]["c"] = classifiers["CSRP"]["f"].predict(X_test)
-# Evaluate
-results.loc["CSRP"] = 0
-results.loc["CSRP", measures.keys()] = \
-    [measures[measure](y_test, classifiers["CSRP"]["c"]) for measure in measures.keys()]
-results["sav_score"].loc["CSRP"] = savings_score(y_test, classifiers["CSRP"]["c"], cost_mat_test)
-results["cost_loss"].loc["CSRP"] = cost_loss(y_test, classifiers["CSRP"]["c"], cost_mat_test)
-
-CostSensitiveClassifiers = {"RFC": {"f": CostSensitiveRandomPatchesClassifier()}}
-
-for model in CostSensitiveClassifiers.keys():
-    CostSensitiveClassifiers[model]["f"].fit(X_train, y_train, cost_mat_train)
-    # Predict
-    CostSensitiveClassifiers[model]["f"] = CostSensitiveClassifiers[model]["f"].predict(X_test)
-    print(savings_score(y_test, CostSensitiveClassifiers[model]["f"], cost_mat_test))
-    print(cost_loss(y_test, CostSensitiveClassifiers[model]["f"], cost_mat_test))
-
-print(results)
+# ci_models = classifiers.keys()
+#
+# for model in list(ci_models):
+#     # ---------------Cost Minimazation-----------------------
+#     # Model BMR
+#     classifiers[model + "-BMR"] = {"f": BayesMinimumRiskClassifier()}  # BMR
+#     # Fit
+#     classifiers[model + "-BMR"]["f"].fit(y_test, classifiers[model]["p"])
+#     # Predict
+#     classifiers[model + "-BMR"]["c"] = classifiers[model + "-BMR"]["f"].predict(classifiers[model]["p"], cost_mat_test)
+#     # Evaluate
+#     results.loc[model + "-BMR"] = 0
+#     results.loc[model + "-BMR", measures.keys()] = \
+#         [measures[measure](y_test, classifiers[model + "-BMR"]["c"]) for measure in measures.keys()]
+#     results["sav_score"].loc[model + "-BMR"] = savings_score(y_test, classifiers[model + "-BMR"]["c"], cost_mat_test)
+#     results["cost_loss"].loc[model + "-BMR"] = cost_loss(y_test, classifiers[model + "-BMR"]["c"], cost_mat_test)
+#     # Model TO
+#     classifiers[model + "-TO"] = {"f": ThresholdingOptimization()}  #
+#     # Fit
+#     classifiers[model + "-TO"]["f"].fit(classifiers[model]["ptrain"], cost_mat_train, y_train)  # Classifier TO
+#     # Predict
+#     classifiers[model + "-TO"]["c"] = classifiers[model + "-TO"]["f"].predict(classifiers[model]["p"])  # Classifier TO
+#     # Evaluate
+#     results.loc[model + "-TO"] = 0
+#     results.loc[model + "-TO", measures.keys()] = \
+#         [measures[measure](y_test, classifiers[model + "-TO"]["c"]) for measure in measures.keys()]
+#     results["sav_score"].loc[model + "-TO"] = savings_score(y_test, classifiers[model + "-TO"]["c"], cost_mat_test)
+#     results["cost_loss"].loc[model + "-TO"] = cost_loss(y_test, classifiers[model + "-TO"]["c"], cost_mat_test)
+#
+# classifiers["CSRP"] = {"f": CostSensitiveRandomPatchesClassifier(combination='weighted_voting')}
+# # Fit
+# classifiers["CSRP"]["f"].fit(X_train, y_train, cost_mat_train)
+# # Predict
+# classifiers["CSRP"]["c"] = classifiers["CSRP"]["f"].predict(X_test)
+# # Evaluate
+# results.loc["CSRP"] = 0
+# results.loc["CSRP", measures.keys()] = \
+#     [measures[measure](y_test, classifiers["CSRP"]["c"]) for measure in measures.keys()]
+# results["sav_score"].loc["CSRP"] = savings_score(y_test, classifiers["CSRP"]["c"], cost_mat_test)
+# results["cost_loss"].loc["CSRP"] = cost_loss(y_test, classifiers["CSRP"]["c"], cost_mat_test)
+#
+# CostSensitiveClassifiers = {"RFC": {"f": CostSensitiveRandomPatchesClassifier()}}
+#
+# for model in CostSensitiveClassifiers.keys():
+#     CostSensitiveClassifiers[model]["f"].fit(X_train, y_train, cost_mat_train)
+#     # Predict
+#     CostSensitiveClassifiers[model]["f"] = CostSensitiveClassifiers[model]["f"].predict(X_test)
+#     print(savings_score(y_test, CostSensitiveClassifiers[model]["f"], cost_mat_test))
+#     print(cost_loss(y_test, CostSensitiveClassifiers[model]["f"], cost_mat_test))
+#
+# print(results)
